7afb5551eccd99cb89a9a9d186cef78668c56b9b
MasterPast <igory733@gmail.com>
2023-12-13 23:51:48 +0200

    part4

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-main.py
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class FriendsRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'Application/json')
+        self.wfile.write('hello epta')
+
+        json.dump(response, wfile)
+
+
+HTTPServer(('localhost',))
