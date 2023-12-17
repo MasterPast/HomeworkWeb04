db9b88a914f72fc099897538a69635ad10c86a18
MasterPast <igory733@gmail.com>
2023-12-17 11:03:16 +0200

    last

--- main.py
+++ main.py
@@ -1,104 +1,102 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from threading import Thread
 import json
-import urllib.parse
-import mimetypes
-import pathlib
+import logging
+import os
 import socket
 from datetime import datetime
+from flask import Flask, redirect, render_template, request
+from threading import Thread
 
-HOST = '127.0.0.1'
+HOST = 'localhost'
 SOCKET_PORT = 5000
-HTTP_PORT = 3000
 
+data_dict = {}
 
-class HttpHandler(BaseHTTPRequestHandler):
+app = Flask(__name__)
 
-    def do_GET(self):
-        pr_url = urllib.parse.urlparse(self.path)
-        if pr_url.path == '/':
-            self.send_html_file('index.html')
-        elif pr_url.path == '/message':
-            self.send_html_file('message.html')
-        else:
-            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
-                self.send_static()
-            else:
-                self.send_html_file('error.html')
+@app.errorhandler(404)
+def my_error_page(error):
+    return render_template('error.html')
 
+@app.route('/')
+def index_page_get():
+    return render_template('index.html')
 
-    def do_POST(self):
+@app.route('/home')
+def home_page_get():
+    return render_template('index.html')
 
-        data = self.rfile.read(int(self.headers['Content-Length']))
-        data_parse = urllib.parse.unquote_plus(data.decode())
-        data_dict = {key: value for key, value in [
-            el.split('=') for el in data_parse.split('&')]}
-        
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
-            client.connect((HOST, SOCKET_PORT))
-            json_data_dict = json.dumps(data_dict).encode()
-            client.sendto(json_data_dict, (HOST, SOCKET_PORT))
-        
-        self.send_response(302)
-        self.send_header('Location', '/')
-        self.end_headers()
+@app.route('/message', methods=['GET'])
+def message_page_get():
+    return render_template('message.html')
 
-
-    def send_html_file(self, filename, status=200):
-
-        self.send_response(status)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        with open(filename, 'rb') as fd:
-            self.wfile.write(fd.read())
-
-
-    def send_static(self):
-
-        self.send_response(200)
-        mt = mimetypes.guess_type(self.path)
-        if mt:
-            self.send_header("Content-type", mt[0])
-        else:
-            self.send_header("Content-type", 'text/plain')
-        self.end_headers()
-        with open(f'.{self.path}', 'rb') as file:
-            self.wfile.write(file.read())
+@app.route('/message', methods=['POST'])
+def message_page_post():
+    username = request.form['username']
+    message = request.form['message']
+    data_dict['username'] = username
+    data_dict['message'] = message
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
+        client.connect((HOST, SOCKET_PORT))
+        json_data_dict = json.dumps(data_dict).encode()
+        client.sendto(json_data_dict, (HOST, SOCKET_PORT))
+    
+    logging.debug('Data_dict was send')
+    return redirect('/')
 
 
 def socket_server():
 
+    logging.debug('socket_server starting...')
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
         server.bind((HOST, SOCKET_PORT))
 
         while True:
+
             data, address = server.recvfrom(1024)
             decode_data = json.loads(data)
             dict_time=str(datetime.now())
+            logging.debug('Start receiving')
 
-            with open('storage\data.json', 'r') as rf:
+            if not os.path.isdir("storage"):
+                logging.debug('Directory STORAGE not found. Creating STORAGE...')
+                os.mkdir('storage')
+            if not os.path.isfile("storage/data.json"):
+                null_dict = {}
+                logging.debug('File not found. Creating file...')
+    
+                with open('storage/data.json', "w") as wf:
+                    json.dump(null_dict, wf, indent=2)
+            
+            with open('storage/data.json', 'r') as rf:
                 file_json_dict = json.load(rf)
                 file_json_dict[dict_time] = decode_data
-
-            with open('storage\data.json', "w") as wf:
+            
+            logging.debug('Start reading from file')
+            
+            with open('storage/data.json', "w") as wf:
                 json.dump(file_json_dict, wf, indent=2)
+            
+            logging.debug('Start saving to file')
+    
+def run():
 
-
-def run(server_class=HTTPServer, handler_class=HttpHandler):
-
-    server_address = (HOST, HTTP_PORT)
-    server = server_class(server_address, handler_class)
-    server_http = Thread(target=server.serve_forever)
-    server_socket = Thread(target=socket_server)
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    logging.debug('Starting app')
+    logging.debug('Creating threads')
 
     try:
-        server_http.start()
+        logging.debug('Starting servers...')
+        server_socket = Thread(target=socket_server)
         server_socket.start()
+        server_http = Thread(target=app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4000))))
+        server_http.start()
+    
+        logging.debug('Servers sucessfully started.')
     except KeyboardInterrupt:
         server_http.server_close()
         server_socket.server_close()
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     run()
